e8cc1f12b2378f253bca09dc2d192479302321f9
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 15:49:17 +0300

    Rewrite GraphicsMagick with ES6

--- src/GraphicsMagick.js
+++ src/GraphicsMagick.js
@@ -1,151 +1,127 @@
-var gm = require('gm');
-var Promise = require('bluebird');
-var util = require('util');
-var BaseImage = require('./BaseImage');
+import gm from 'gm';
+import Promise from 'bluebird';
+import BaseImage from './BaseImage';
 
-util.inherits(GraphicsMagick, BaseImage);
+export default class GraphicsMagick extends BaseImage {
+  constructor(...args) {
+    super(...args);
 
-/**
- * Create GM instance
- * @constructor
- */
-function GraphicsMagick() {
-  BaseImage.apply(this, arguments);
+    this.setProvider(gm);
+  }
 
-  this.setProvider(gm);
+  /**
+   * Get title from IPTC
+   * @param {String|Buffer|Stream} image Source image
+   * @returns {Promise}
+   */
+  getTitle(image) {
+    return new Promise((resolve, reject) => {
+      this.getProvider()(image).identify('%[IPTC:2:05]', (error, metadata) => error ? reject(error) : resolve(metadata.trimRight()));
+    });
+  }
+
+  /**
+   * Get description from IPTC
+   * @param {String|Buffer|Stream} image Source image
+   * @returns {Promise}
+   */
+  getDescription(image) {
+    return new Promise((resolve, reject) => {
+      this.getProvider()(image).identify('%[IPTC:2:120]', (error, metadata) => error ? reject(error) : resolve(metadata.trimRight()));
+    });
+  }
+
+  /**
+   * Get keywords from IPTC
+   * @param {String|Buffer|Stream} image Source image
+   * @returns {Promise}
+   */
+  getKeywords(image) {
+    return new Promise((resolve, reject) => {
+      this.getProvider()(image).identify('%[IPTC:2:25]', (error, metadata) => error ? reject(error) : resolve(metadata.trimRight().split(';').join(',')));
+    });
+  }
+
+  /**
+   * Get category from IPTC
+   * @param {String|Buffer|Stream} image Source image
+   * @returns {Promise}
+   */
+  getCategory(image) {
+    return new Promise((resolve, reject) => {
+      this.getProvider()(image).identify('%[IPTC:2:15]', (error, metadata) => error ? reject(error) : resolve(metadata.trimRight()));
+    });
+  }
+
+  /**
+   * Get image format
+   * @param {String|Buffer|Stream} image Source image
+   * @returns {Promise}
+   */
+  getFormat(image) {
+    return new Promise((resolve, reject) => {
+      this.getProvider()(image).identify((error, data) => error ? reject(error) : resolve(data.format));
+    });
+  }
+
+  /**
+   * Get IPTC information from image
+   * @param {String|Buffer|Stream} image Source image
+   * @returns {Promise}
+   */
+  iptc(image) {
+    return Promise.all([
+      this.getTitle(image),
+      this.getDescription(image),
+      this.getKeywords(image),
+      this.getCategory(image),
+      this.getFormat(image)
+    ]).spread((title, description, keywords, category, format) => {
+      Promise.resolve({title, description, keywords, category, format});
+    });
+  }
+
+  /**
+   * Resize image
+   * @param {String|Buffer|Stream} image Source image
+   * @param {Object} [_config] Configuration object
+   * @returns {Promise}
+   */
+  resize(image, _config) {
+    let config = _config || {};
+
+    return new Promise((resolve, reject) => {
+      this.getProvider()(image)
+        .resize(config.width || 200, config.height, config.direction || '>')
+        .setFormat(config.format || 'jpg')
+        .toBuffer((error, buffer) => error ? reject(error) : resolve(buffer));
+    });
+  }
+
+  /**
+   * Crop image
+   * @param {String|Buffer|Stream} image Source image
+   * @param {Object} [_config] Configuration object
+   * @returns {Promise}
+   */
+  crop(image, _config) {
+    let config = _config || {};
+
+    return new Promise((resolve, reject) => {
+      this.getProvider()(image)
+        .crop(config.width || '100%', config.height || '100%', config.x || 0, config.y || 0)
+        .setFormat(config.format || 'jpg')
+        .toBuffer((error, buffer) => error ? reject(error) : resolve(buffer));
+    });
+  }
+
+  /**
+   * Create thumbnail for image
+   * @param {String|Buffer|Stream} image Source image
+   * @param {Object} [_config]
+   * @returns {Promise}
+   */
+  thumbnail(image, _config) {
+    this.resize(image, _config);
+  }
 }
-
-/**
- * Get title from IPTC
- * @param {String|Buffer|Stream} image Source image
- * @returns {Promise}
- */
-GraphicsMagick.prototype.getTitle = function (image) {
-  return new Promise(function (resolve, reject) {
-    this.getProvider()(image).identify('%[IPTC:2:05]', function (error, metadata) {
-      return error ? reject(error) : resolve(metadata.trimRight());
-    });
-  }.bind(this));
-};
-
-/**
- * Get description from IPTC
- * @param {String|Buffer|Stream} image Source image
- * @returns {Promise}
- */
-GraphicsMagick.prototype.getDescription = function (image) {
-  return new Promise(function (resolve, reject) {
-    this.getProvider()(image).identify('%[IPTC:2:120]', function (error, metadata) {
-      return error ? reject(error) : resolve(metadata.trimRight());
-    });
-  }.bind(this));
-};
-
-/**
- * Get keywords from IPTC
- * @param {String|Buffer|Stream} image Source image
- * @returns {Promise}
- */
-GraphicsMagick.prototype.getKeywords = function (image) {
-  return new Promise(function (resolve, reject) {
-    this.getProvider()(image).identify('%[IPTC:2:25]', function (error, metadata) {
-      return error ? reject(error) : resolve(metadata.trimRight().split(';').join(','));
-    });
-  }.bind(this));
-};
-
-/**
- * Get category from IPTC
- * @param {String|Buffer|Stream} image Source image
- * @returns {Promise}
- */
-GraphicsMagick.prototype.getCategory = function (image) {
-  return new Promise(function (resolve, reject) {
-    this.getProvider()(image).identify('%[IPTC:2:15]', function (error, metadata) {
-      return error ? reject(error) : resolve(metadata.trimRight());
-    });
-  }.bind(this));
-};
-
-/**
- * Get image format
- * @param {String|Buffer|Stream} image Source image
- * @returns {Promise}
- */
-GraphicsMagick.prototype.getFormat = function (image) {
-  return new Promise(function (resolve, reject) {
-    this.getProvider()(image).identify(function (error, data) {
-      return error ? reject(error) : resolve(data.format);
-    });
-  }.bind(this));
-};
-
-/**
- * Get IPTC information from image
- * @param {String|Buffer|Stream} image Source image
- * @returns {Promise}
- */
-GraphicsMagick.prototype.iptc = function (image) {
-  return Promise.all([
-    this.getTitle(image),
-    this.getDescription(image),
-    this.getKeywords(image),
-    this.getCategory(image),
-    this.getFormat(image)
-  ]).spread(function (title, description, keywords, category, format) {
-    return Promise.resolve({
-      title: title,
-      description: description,
-      keywords: keywords,
-      category: category,
-      format: format
-    });
-  });
-};
-
-/**
- * Resize image
- * @param {String|Buffer|Stream} image Source image
- * @param {Object} [_config] Configuration object
- * @returns {Promise}
- */
-GraphicsMagick.prototype.resize = function (image, _config) {
-  var config = _config || {};
-
-  return new Promise(function (resolve, reject) {
-    this.getProvider()(image)
-      .resize(config.width || 200, config.height, config.direction || '>')
-      .setFormat(config.format || 'jpg')
-      .toBuffer(function (error, buffer) {
-        return error ? reject(error) : resolve(buffer);
-      });
-  }.bind(this));
-};
-
-/**
- * Crop image
- * @param {String|Buffer|Stream} image Source image
- * @param {Object} [_config] Configuration object
- * @returns {Promise}
- */
-GraphicsMagick.prototype.crop = function (image, _config) {
-  var config = _config || {};
-
-  return new Promise(function (resolve, reject) {
-    this.getProvider()(image)
-      .crop(config.width || '100%', config.height || '100%', config.x || 0, config.y || 0)
-      .setFormat(config.format || 'jpg')
-      .toBuffer(function (error, buffer) {
-        return error ? reject(error) : resolve(buffer);
-      });
-  }.bind(this));
-};
-
-/**
- * Create thumbnail for image
- * @param {String|Buffer|Stream} image Source image
- * @returns {Promise}
- */
-GraphicsMagick.prototype.thumbnail = this.resize;
-
-module.exports = GraphicsMagick;
